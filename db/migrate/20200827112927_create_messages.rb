7a3123224904c7fd251f6feeaca7f13a096e5e71
林雄平 <yuhei@hayashiyuuheinoMacBook-Pro.local>
2020-08-28 12:19:18 +0900

    空のメッセージが送信できないバリデーションの設定

--- db/migrate/20200827112927_create_messages.rb
+++ db/migrate/20200827112927_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.string :content
+      t.string :content 
       t.references :user ,foreign: true
       t.references :room ,foreign: true
       t.timestamps
